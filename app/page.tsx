c27d239df38bad24952822bde54cc38f0ae94d85
bereketdinku <bereketdinku80@gmail.com>
2024-02-27 02:58:13 -0800

    pa

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,36 @@
-import Image from 'next/image'
-import Container from './components/Container'
-import HomeBanner from './components/HomeBanner'
-import ProductCard from './components/products/ProductCard'
-import getProducts, { IProductParams } from '@/actions/getProducts'
-import NullData from './components/NullData'
-interface HomeProps{
-searchParams:IProductParams
+import Container from "./components/Container";
+import HomeBanner from "./components/HomeBanner";
+import ProductCard from "./components/products/ProductCard";
+import getProducts, { IProductParams } from "@/actions/getProducts";
+import NullData from "./components/NullData";
+interface HomeProps {
+  searchParams: IProductParams;
 }
-export default async function Home({searchParams}:HomeProps) {
-  const products= await getProducts(searchParams)
-  if(products.length===0){
-    return <NullData title='Oops! No products found. Click "All" to clear filters'/>
+export default async function Home({ searchParams }: HomeProps) {
+  const products = await getProducts(searchParams);
+  if (products.length === 0) {
+    return (
+      <NullData title='Oops! No products found. Click "All" to clear filters' />
+    );
   }
-  function shuffleArray(array:any){
-    for(let i=array.length-1;i>0;i--){
-      const j=Math.floor(Math.random()*(i+1));
-      [array[i],array[j]]=[array[j],array[i]]
+  function shuffleArray(array: any) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
   }
-  const shuffledProducts=shuffleArray(products)
+  const shuffledProducts = shuffleArray(products);
   return (
     <Container>
       <div>
-        <HomeBanner/>
+        <HomeBanner />
       </div>
-      <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl-grid-cols-5 2xl:grid-cols-6 gap-8'>
-        {shuffledProducts.map((product:any)=>{
-          return <ProductCard data={product}/>
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl-grid-cols-5 2xl:grid-cols-6 gap-8">
+        {shuffledProducts.map((product: any, index: any) => {
+          return <ProductCard data={product} key={index} />;
         })}
       </div>
     </Container>
-  )
+  );
 }
