28bd6221bd9e6f12fdf9f81b6a5cea16e80594bc
bereketdinku <bereketdinku80@gmail.com>
2024-02-27 03:18:57 -0800

    order

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -5,21 +5,24 @@ import { getCurrentUser } from "@/actions/getCurrentUser";
 import NullData from "@/app/components/NullData";
 import getOrdersByUserId from "@/actions/getOrdersByUser";
 
-const Orders = async() => {
-    const currentUser=await getCurrentUser()
+const Orders = async () => {
+  const currentUser = await getCurrentUser();
 
-    const orders=await getOrdersByUserId(currentUser!.id)
-    if(!currentUser ){
-        return <NullData title="Oops! Access denied"/>
-    }
-    if(!orders){
-        return <NullData title="No orders yet..."/>
-    }
-    return ( <div className="pt-8">
-<Container>
-    <OrderClient orders={orders}/>
-</Container>
-    </div> );
-}
- 
+  if (!currentUser) {
+    return <NullData title="Oops! Access denied" />;
+  }
+  const orders = await getOrdersByUserId(currentUser.id);
+
+  if (!orders) {
+    return <NullData title="No orders yet..." />;
+  }
+  return (
+    <div className="pt-8">
+      <Container>
+        <OrderClient orders={orders} />
+      </Container>
+    </div>
+  );
+};
+
 export default Orders;
